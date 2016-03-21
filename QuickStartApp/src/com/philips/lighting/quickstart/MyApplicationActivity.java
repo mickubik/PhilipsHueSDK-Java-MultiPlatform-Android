d1bc2d0acb18a363ed468e6d38927184c3d7fd17
Michael <michaelrkubik@gmail.com>
2016-03-20 17:20:15 -0800

    Turn lights on or off when app starts
    If any lights are on they are all turned off. Otherwise all the lights
    are turned on when the app starts.

--- QuickStartApp/src/com/philips/lighting/quickstart/MyApplicationActivity.java
+++ QuickStartApp/src/com/philips/lighting/quickstart/MyApplicationActivity.java
@@ -13,8 +13,10 @@ import android.widget.Button;
 
 import com.philips.lighting.hue.listener.PHLightListener;
 import com.philips.lighting.hue.sdk.PHHueSDK;
+import com.philips.lighting.hue.sdk.utilities.PHUtilities;
 import com.philips.lighting.model.PHBridge;
 import com.philips.lighting.model.PHBridgeResource;
+import com.philips.lighting.model.PHBridgeResourcesCache;
 import com.philips.lighting.model.PHHueError;
 import com.philips.lighting.model.PHLight;
 import com.philips.lighting.model.PHLightState;
@@ -37,6 +39,8 @@ public class MyApplicationActivity extends Activity {
         setTitle(R.string.app_name);
         setContentView(R.layout.activity_main);
         phHueSDK = PHHueSDK.create();
+
+
         Button randomButton;
         randomButton = (Button) findViewById(R.id.buttonRand);
         randomButton.setOnClickListener(new OnClickListener() {
@@ -48,6 +52,67 @@ public class MyApplicationActivity extends Activity {
 
         });
 
+        if (aLightsIsOn()){
+            System.out.println("a light is on");
+            turnLightsOff();
+
+        } else {
+            System.out.println("no lights on");
+
+            turnLightsOn(9000);
+        }
+
+
+    }
+
+    public boolean aLightsIsOn() {
+        boolean aLightIsOn = false;
+
+        PHBridge bridge = phHueSDK.getSelectedBridge();
+        List<PHLight> allLights = bridge.getResourceCache().getAllLights();
+
+        for (PHLight light : allLights) {
+
+            light.getLastKnownLightState();
+            PHLightState state = light.getLastKnownLightState();
+//            int color = PHUtilities.colorFromXY(new float[]{state.getX(), state.getY()}, light.getModelNumber());
+            if (state.isOn()) {
+                return true;
+            }
+
+//            System.out.println("color: " + color);
+//            System.out.println("state.isOn(): " + state.isOn());
+        }
+        return false;
+    }
+
+    public void turnLightsOn(int i) {
+        PHBridge bridge = phHueSDK.getSelectedBridge();
+
+        List<PHLight> allLights = bridge.getResourceCache().getAllLights();
+
+        for (PHLight light : allLights) {
+            PHLightState lightState = new PHLightState();
+            lightState.setOn(true);
+            lightState.setHue(i);
+            // To validate your lightstate is valid (before sending to the bridge) you can use:
+            // String validState = lightState.validateState();
+            bridge.updateLightState(light, lightState, listener);
+            //  bridge.updateLightState(light, lightState);   // If no bridge response is required then use this simpler form.
+        }
+    }
+
+    public void turnLightsOff() {
+        PHBridge bridge = phHueSDK.getSelectedBridge();
+
+        List<PHLight> allLights = bridge.getResourceCache().getAllLights();
+
+        for (PHLight light : allLights) {
+            PHLightState lightState = new PHLightState();
+            lightState.setOn(false);
+            bridge.updateLightState(light, lightState, listener);
+            //bridge.updateLightState(light, lightState);   // If no bridge response is required then use this simpler form.
+        }
     }
 
     public void randomLights() {
